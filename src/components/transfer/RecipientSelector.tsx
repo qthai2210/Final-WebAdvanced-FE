fb2b5d4754893ee8f23826fa4431cd89716d072f
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2024-12-19 21:59:48 +0700

    Modify code find accountNumber

--- src/components/transfer/RecipientSelector.tsx
+++ src/components/transfer/RecipientSelector.tsx
@@ -77,7 +77,7 @@ const RecipientSelector: React.FC<Props> = ({ onSelect, accountNumber }) => {
             <h3 className="font-medium text-gray-700">Account Information</h3>
             <div className="mt-1 text-sm text-gray-600">
               <p>Account Number: {selectedRecipient.accountNumber}</p>
-              <p>Type: {selectedRecipient.type}</p>
+              <p>Nickname: {selectedRecipient.nickname}</p>
             </div>
           </div>
         )}
